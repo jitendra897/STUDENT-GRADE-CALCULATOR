5a115c04d471aa39d20fa430757145aa26586390
jitendra singh <126086686+jitendra897@users.noreply.github.com>
2023-09-14 11:38:14 +0530

    Add files via upload

---
+++ GradeCalculator.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        if (numSubjects <= 0) {
+            System.out.println("Invalid number of subjects. Please enter a positive number.");
+            scanner.close();
+            return;
+        }
+
+        int[] subjectMarks = new int[numSubjects];
+        int totalMarks = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            int marks = scanner.nextInt();
+
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks. Please enter marks between 0 and 100.");
+                scanner.close();
+                return;
+            }
+
+            subjectMarks[i] = marks;
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+        String grade = calculateGrade(averagePercentage);
+
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    public static String calculateGrade(double percentage) {
+        if (percentage >= 90) {
+            return "A+";
+        } else if (percentage >= 80) {
+            return "A";
+        } else if (percentage >= 70) {
+            return "B";
+        } else if (percentage >= 60) {
+            return "C";
+        } else if (percentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
